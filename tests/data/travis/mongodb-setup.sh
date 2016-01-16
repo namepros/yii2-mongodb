647e2cec86bf4194f6d51944fa813b9d845189a8
Paul Buonopane <Zenexer@users.noreply.github.com>
2016-01-15 20:35:05 -0500

    Fixes for MongoDB version detection

--- tests/data/travis/mongodb-setup.sh
+++ tests/data/travis/mongodb-setup.sh
@@ -19,8 +19,13 @@ fi
 echo "MongoDB Server version:"
 mongod --version
 
-echo "MongoDB PHP Extension version:"
-php -i |grep mongo -4 |grep -2 Version
+if ! (php --version | grep -i HipHop > /dev/null); then
+  echo "HHVM version:"
+  hhvm --version
+else
+  echo "MongoDB PHP Extension version:"
+  php -i | grep '^mongodb version => '
+fi
 
 # enable text search
 mongo --eval 'db.adminCommand( { setParameter: true, textSearchEnabled : true})'
